a31780e436a57d4f93b5a4fedd403c5e66e3e181
Dipak Poudel <pdl.dipak@gmail.com>
2020-11-21 08:46:02 +0100

    react native color palate workshp from frontendmaster

--- screens/Home.js
+++ screens/Home.js
@@ -1,86 +1,69 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+  StyleSheet,
+  FlatList,
+  RefreshControl,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import PalettePreview from '../components/PalettePreview';
-/* 
-const SOLARIZED = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-  { colorName: 'Base2', hexCode: '#eee8d5' },
-  { colorName: 'Base3', hexCode: '#fdf6e3' },
-  { colorName: 'Yellow', hexCode: '#b58900' },
-  { colorName: 'Orange', hexCode: '#cb4b16' },
-  { colorName: 'Red', hexCode: '#dc322f' },
-  { colorName: 'Magenta', hexCode: '#d33682' },
-  { colorName: 'Violet', hexCode: '#6c71c4' },
-  { colorName: 'Blue', hexCode: '#268bd2' },
-  { colorName: 'Cyan', hexCode: '#2aa198' },
-  { colorName: 'Green', hexCode: '#859900' },
-];
 
-const RAINBOW = [
-  { colorName: 'Red', hexCode: '#FF0000' },
-  { colorName: 'Orange', hexCode: '#FF7F00' },
-  { colorName: 'Yellow', hexCode: '#FFFF00' },
-  { colorName: 'Green', hexCode: '#00FF00' },
-  { colorName: 'Violet', hexCode: '#8B00FF' },
-];
-
-const FRONTEND_MASTERS = [
-  { colorName: 'Red', hexCode: '#c02d28' },
-  { colorName: 'Black', hexCode: '#3e3e3e' },
-  { colorName: 'Grey', hexCode: '#8a8a8a' },
-  { colorName: 'White', hexCode: '#ffffff' },
-  { colorName: 'Orange', hexCode: '#e66225' },
-];
-
-const COLOR_PALETTES = [
-  { paletteName: 'Solarized', colors: SOLARIZED },
-  { paletteName: 'Frontend Masters', colors: FRONTEND_MASTERS },
-  { paletteName: 'Rainbow', colors: RAINBOW },
-]; */
 const URL = 'https://color-palette-api.kadikraman.now.sh/palettes';
 
-const Home = ({ navigation }) => {
-  const [colorPalettes, setColorPalettes] = useState([]);
+const Home = ({ navigation, route }) => {
+  const newPalette = route.params ? route.params.newPalette : null;
+  const [palettes, setPalettes] = useState([]);
   const [isRefreshing, setIsRefreshing] = useState(false);
 
   const handleFetchPalettes = useCallback(async () => {
     const response = await fetch(URL);
     if (response.ok) {
       const palettes = await response.json();
-      setColorPalettes(palettes);
+      setPalettes(palettes);
     }
   }, []);
 
   useEffect(() => {
     handleFetchPalettes();
   }, []);
+
   const handleRefresh = useCallback(async () => {
     setIsRefreshing(true);
     await handleFetchPalettes();
     setTimeout(() => {
       setIsRefreshing(false);
     }, 1000);
-  }, []);
+  });
+
+  useEffect(() => {
+    if (newPalette) {
+      setPalettes((current) => [newPalette, ...current]);
+    }
+  }, [newPalette]);
+
   return (
-    <FlatList
-      style={styles.list}
-      // data={COLOR_PALETTES}
-      data={colorPalettes}
-      keyExtractor={(item) => item.paletteName}
-      renderItem={({ item }) => (
-        <PalettePreview
-          onPress={() => navigation.push('ColorPalette', item)}
-          palette={item}
-        />
-      )}
-      refreshing={isRefreshing}
-      onRefresh={handleRefresh}
-    />
+    <>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('AddNewPalette')}
+      >
+        <Text style={styles.buttonText}>Add a color scheme</Text>
+      </TouchableOpacity>
+      <FlatList
+        style={styles.list}
+        data={palettes}
+        keyExtractor={(item) => item.paletteName}
+        renderItem={({ item }) => (
+          <PalettePreview
+            onPress={() => navigation.push('ColorPalette', item)}
+            palette={item}
+          />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={isRefreshing} onRefresh={handleRefresh} />
+        }
+      />
+    </>
   );
 };
 
@@ -89,6 +72,16 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     backgroundColor: 'white',
+  },
+  button: {
+    height: 50,
+    backgroundColor: 'white',
+    padding: 10,
+  },
+  buttonText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'teal',
   },
 });
 
